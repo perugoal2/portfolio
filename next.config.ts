5be70f3ca26a5526ff59c8a49ef14ffd44b6dab5
perugoal2 <perugoal2@gmail.com>
2026-04-17 02:48:06 +0800

    Update image rendering

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig: NextConfig = {
   output: 'export', // Static HTML export
   images: { unoptimized: true },
-  assetPrefix: isProd ? '/portfolio/' : '',
-  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: '/portfolio/',
+  basePath: '/portfolio',
 };
 
 export default nextConfig;
