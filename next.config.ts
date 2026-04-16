1faa3f7ec6b80b76e55a3fb9b7b3e5a18b6ae867
perugoal2 <perugoal2@gmail.com>
2026-04-17 02:55:32 +0800

    Update deployment

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 const nextConfig: NextConfig = {
   output: 'export', // Static HTML export
   images: { unoptimized: true },
-  assetPrefix: isGitHubPages ? '/portfolio' : '',
+  assetPrefix: isGitHubPages ? '/portfolio/' : '',
   basePath: isGitHubPages ? '/portfolio' : '',
 };
 
